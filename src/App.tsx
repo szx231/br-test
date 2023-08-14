21915b4cb278aece9af0d79311b33a9ea511a045
scrp <scrappyjs666@gmail.com>
2023-08-14 04:45:48 +0300

    added App styles and logic, app it is a main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import styles from './App.module.css';
+import { InputMask } from './components/InputMask';
+import { Select } from './components/Select';
+import { Range } from './components/Range';
+import { CheckBox } from './components/CheckBox';
+import darkButtonImage from '../public/darkBtn.svg';
+import blueButtonImage from '../public/blueBtn.svg';
+import { useAppDispatch, useAppSelector } from './hooks';
 import { getData } from './store/getData/thunk';
-import { useAppDispatch } from './hooks';
+import { selectData } from './store/getData/selectors';
+import { Social } from './components/Social';
+import { tarif } from './store/tarif/selectors';
 
 function App() {
   const dispatch = useAppDispatch();
-  const [count, setCount] = useState(0);
+  const { data, loading, error } = useAppSelector(selectData);
+  const { allFields } = useAppSelector(tarif);
+
+  const resultSumm = `${
+    allFields.socialSumm && allFields.wifi
+      ? Number(allFields.socialSumm) + Number(allFields.wifi.replace(/[^\d]/g, ''))
+      : 0
+  } ₽`;
 
   useEffect(() => {
     dispatch(getData());
-  }, []);
+  }, [dispatch]);
 
-  return <></>;
+  if (loading) return <div>loading</div>;
+  if (error) return <div>error</div>;
+
+  return (
+    <>
+      {data && (
+        <div className={styles.form}>
+          <h1 className={styles.title}>Настройте тариф</h1>
+          <InputMask />
+          <Select options={data.operators} />
+          <Range title={'Минуты'} color={'var(--color-accent)'} data={data.minutes} buttonImage={blueButtonImage} />
+          <Range title={'Интернет'} color={'black'} data={data.Internet} buttonImage={darkButtonImage} />
+          <CheckBox firstTitle={data.wifi[0]} secondTitle={data.wifi[1]} />
+          <Social social={data.social} />
+          <button type="button" onClick={() => alert(JSON.stringify(allFields))} className={styles.button}>
+            <span className={styles.typography}>{resultSumm}</span>
+            <span className={styles.commonText}>в месяц</span>
+          </button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
